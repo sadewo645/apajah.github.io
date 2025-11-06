f9033b00355a43fd6573723f9e6a7a84b1850db8
sadewo645 <jamrosadewo@gmail.com>
2025-11-06 22:44:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AppLayout } from './components/AppLayout'
 import { DashboardPage } from './pages/Dashboard'
 import { PerkebunanPage } from './pages/Perkebunan'
 import { PabrikPage } from './pages/Pabrik'
-import { PerusahaanPage } from './pages/Perusahaan'
+import Perkebunan from './pages/Perkebunan'
 import { PanduanPage } from './pages/Panduan'
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <AppLayout>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
-        <Route path="/perkebunan" element={<PerkebunanPage />} />
+        <Route path="/perkebunan" element={<Perkebunan />} />
         <Route path="/pabrik" element={<PabrikPage />} />
         <Route path="/perusahaan" element={<PerusahaanPage />} />
         <Route path="/panduan" element={<PanduanPage />} />
